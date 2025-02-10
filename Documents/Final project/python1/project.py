73a871591ad90a41bbc8f46cb3bafd086c53e0c5
elladang <trangdangngoc0707@gmail.com>
2025-02-10 16:37:07 +0700

    Your commit message

--- Documents/Final project/python1/project.py
+++ Documents/Final project/python1/project.py
@@ -5,18 +5,18 @@ có xuất hiện trong tên sản phẩm hoặc mô tả sản phẩm thì in s
 hình như sau:
 
 '''
-# import csv
-# with open('san_pham.csv', 'r', encoding='utf-8') as file: 
-#     nd = csv.reader(file)
-#     header = next(nd)
-#     kw = input('Nhập từ khoá cần tìm: ')
-#     found = [ i for i in nd if kw in i[1].lower() or kw in i[3].lower()]
-# print(f"TÌM THẤY {len(found)} SẢN PHẨM")
-# print("-"*70)
-# print("MÃ SP".ljust(12), "TÊN SẢN PHẨM".ljust(50), "GIÁ BÁN".ljust(10))
-# print("-"*70)
-# for i in found:
-#     print(f"SP{i[0].ljust(10)}", i[1].ljust(50), i[2].ljust(10))
+import csv
+with open('san_pham.csv', 'r', encoding='utf-8') as file: 
+    nd = csv.reader(file)
+    header = next(nd)
+    kw = input('Nhập từ khoá cần tìm: ')
+    found = [ i for i in nd if kw in i[1].lower() or kw in i[3].lower()]
+print(f"TÌM THẤY {len(found)} SẢN PHẨM")
+print("-"*70)
+print("MÃ SP".ljust(12), "TÊN SẢN PHẨM".ljust(50), "GIÁ BÁN".ljust(10))
+print("-"*70)
+for i in found:
+    print(f"SP{i[0].ljust(10)}", i[1].ljust(50), i[2].ljust(10))
 
 
 # 
